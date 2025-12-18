213d808cd74900357ea5e085b767f27bb36d0baf
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-18 07:08:24 +0000

    container config done

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 121212
+let accountEmail = "nikunj@gmail.com"
+var accountPassword = "22002200"
+accoutnCity = "mumbai"
+
+
+// accountId = 2 // not allowed
+
+
+
+accountEmail = "nk@nk.com"
+accountPassword = "2222222"
+accoutnCity = "pune"    
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accoutnCity])
