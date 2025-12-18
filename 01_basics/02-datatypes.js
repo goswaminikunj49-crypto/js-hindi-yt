c4cca3d890202e2c0655bd0769c2c9712bf867ca
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-18 12:30:09 +0000

    datatypes

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, note browser
+
+
+console.log(3 + 3); // code readability should be high
+
+console.log("nikunj")
+
+
+
+let name = "nikunj"
+let age = 18
+let isLoggedIn = false
+let state; 
+
+
+// number => 2 to the power of 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
