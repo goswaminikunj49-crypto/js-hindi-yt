09b3def5637f06591e89dd4ad0f4bee0ff3a0316
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-19 07:02:56 +0000

    datatypes summery

---
+++ datatypes-summery.js
@@ -0,0 +1,41 @@
+// primitive
+
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigint
+
+//  reference type (non primirive)
+
+// arrays, objects, functions
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 354316498153547899n   
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello world");
+} 
+
+
+console.log(typeof anotherId); 
