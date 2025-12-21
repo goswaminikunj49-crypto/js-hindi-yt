790795d7c12ef134d270804c5466b36c52d86e4f
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-21 13:50:02 +0000

    num and maths

---
+++ 01_basics/06-num_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance.toFixed(1));
+    
+
+const otherNumber = 99.8966 
+
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(11.8));
+// console.log(Math.ceil(11.8));
+// console.log(Math.floor(11.5));
+// console.log(Math.min(11, 5, 8, 3, 20));
+// console.log(Math.max(11, 5, 8, 3, 20));
+
+
+console.log(Math.random()*10 + 1);
