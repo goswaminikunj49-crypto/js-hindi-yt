e77ebe14eb9497911bd5fd695e79ceadbaf9d3cd
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-22 07:33:27 +0000

    dates JS

--- 01_basics/06-num_and_maths.js
+++ 01_basics/06-num_and_maths.js
@@ -25,4 +25,12 @@ const hundred = 1000000
 // console.log(Math.max(11, 5, 8, 3, 20));
 
 
-console.log(Math.random()*10 + 1);
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
