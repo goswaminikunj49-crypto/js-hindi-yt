ea3846321cf446336a316625dd49add2a997b3cc
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-25 07:27:46 +0000

    json

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,78 @@
+// const tinderUser = new Object();
+
+const tinderUser = {}
+
+ tinderUser.id = "123abc"
+tinderUser.name = "nikk"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser); 
+
+ const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "nikunj",
+            lastname: "goswami"
+        }
+    }
+ }
+
+//  console.log(regularUser.fullname.userfullname.firstname.lastname);
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2,}
+// console.log(obj3);
+
+const user =[
+{
+    id: 1,
+    email: "n@gmail.com"
+},
+{
+    id: 1,
+    email: "n@gmail.com"
+},
+{
+    id: 1,
+    email: "n@gmail.com"
+},
+
+]
+user[1].email 
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); 
+
+
+const course = {
+    coursename: "js in hindi",
+    price: 999,
+    courseInstructor: "nikk",
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+// {
+//     name: "nikk",
+//     coursename: "js in hindi",
+//     price: "free,
+// }
