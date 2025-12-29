6f9934e36c5cd929a12d08ff81b01241e46ade63
goswaminikunj49-crypto <goswaminikunj49@gmail.com>
2025-12-29 13:15:56 +0000

    functions

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,33 @@
+// // var c = 300
+// let a = 500
+
+// if (true) {
+//     let a = 15
+//     const b = 20
+//     var c = 30
+
+//     console.log("INNER ", a);
+
+
+// }
+ 
+
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+
+
+function one () {
+    const username = "nikk"
+
+    function two () {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+one()
